9d807330672cce5cff837c9618489f94643eddc1
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-06-15 16:42:33 +0900

    Initial Commit

---
+++ Indieplus_Pohang/Indieplus_PohangApp.swift
@@ -0,0 +1,17 @@
+//
+//  Indieplus_PohangApp.swift
+//  Indieplus_Pohang
+//
+//  Created by GYURI PARK on 2023/06/15.
+//
+
+import SwiftUI
+
+@main
+struct Indieplus_PohangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
