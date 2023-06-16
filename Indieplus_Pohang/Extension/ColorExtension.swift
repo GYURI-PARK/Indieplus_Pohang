c49b19883f2dffcd0240e02f98c872e62c647797
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-06-16 14:25:13 +0900

    #1 [FEAT] ColorExtension 추가

---
+++ Indieplus_Pohang/Extension/ColorExtension.swift
@@ -0,0 +1,20 @@
+//
+//  ColorExtension.swift
+//  Indieplus_Pohang
+//
+//  Created by GYURI PARK on 2023/06/16.
+//
+
+import SwiftUI
+
+extension Color {
+    init(_ hex: UInt, alpha: Double = 1){
+        self.init(
+            .sRGB,
+        red: Double((hex>>16) & 0xFF) / 255,
+            green: Double((hex >> 8) & 0xFF) / 255,
+        blue: Double((hex >> 0) & 0xFF) / 255,
+        opacity: alpha
+        )
+    }
+}
