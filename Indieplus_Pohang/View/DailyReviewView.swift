6cbfa0406bbd1cdf6a8c3b758772e99872bfaec2
Doris <93391058+GYURI-PARK@users.noreply.github.com>
2023-06-17 01:40:08 +0900

    #1 DailyReviewView 추가

---
+++ Indieplus_Pohang/View/DailyReviewView.swift
@@ -0,0 +1,30 @@
+//
+//  DailyReviewView.swift
+//  Indieplus_Pohang
+//
+//  Created by GYURI PARK on 2023/06/17.
+//
+
+import SwiftUI
+
+struct DailyReviewView: View {
+    var body: some View {
+        VStack(alignment: .leading){
+            Text("Daily REVIEW")
+                .foregroundColor(.white)
+            
+            ZStack{
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundColor(Color(0xEF865B))
+                    .opacity(0.5)
+                    .frame(width: 330, height: 120)
+            }
+        }
+    }
+}
+
+struct DailyReviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        DailyReviewView()
+    }
+}
--- Indieplus_Pohang/View/MainView.swift
+++ Indieplus_Pohang/View/MainView.swift
@@ -50,6 +50,7 @@ struct MainView: View {
                         MoviePickerView()
                         
                         
+                        DailyReviewView()
                     }
                 }
             }
