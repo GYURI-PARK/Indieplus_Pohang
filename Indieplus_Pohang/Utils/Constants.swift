21c2046271e6665460d75dba28fb7b930cc874a8
GYURI-PARK <93391058+GYURI-PARK@users.noreply.github.com>
2023-11-16 23:17:34 +0900

    [REFACTOR] #57 모든 기기 사이즈 대응

---
+++ Indieplus_Pohang/Utils/Constants.swift
@@ -0,0 +1,35 @@
+//
+//  Constants.swift
+//  Indieplus_Pohang
+//
+//  Created by GYURI PARK on 2023/11/16.
+//
+
+import Foundation
+import SwiftUI
+
+struct Constants {
+    
+    static var bottomOffset: Double {
+        switch UIScreen.main.bounds.size {
+        case CGSize(width: 430, height: 932): // 14 Pro Max
+            return 370.0
+        case CGSize(width: 393, height: 852): // 14 Pro
+            return 350.0
+        case CGSize(width: 428, height: 926): // 14 Plus, 13 Pro Max, 12 Pro Max
+            return 370.0
+        case CGSize(width: 390, height: 844): // 14, 13 Pro, 13, 12 Pro, 12
+            return 350.0
+        case CGSize(width: 375, height: 667): // SE
+            return 250.0
+        case CGSize(width: 375, height: 812): // 13 Mini, 12 Mini, 11 Pro, XS
+            return 330.0
+        case CGSize(width: 375, height: 780): // 13 Mini, 12 Mini
+            return 330.0
+        case CGSize(width: 414, height: 896): // 11 Pro Max, 11, XS Max, XR
+            return 365.0
+        default:
+            return 250.0
+        }
+    }
+}
--- Indieplus_Pohang/View/SubPage/FastTicketingView.swift
+++ Indieplus_Pohang/View/SubPage/FastTicketingView.swift
@@ -12,19 +12,21 @@ struct FastTicketingView: View {
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        NavigationLink(destination: LoadingWebView(urlToLoad: "https://www.dtryx.com/cinema/main.do?cgid=FE8EF4D2-F22D-4802-A39A-D58F23A29C1E&BrandCd=indieart&CinemaCd=000057", viewModel: viewModel)) {
-            ZStack {
-                Rectangle()
-                    .frame(width: 100, height: 40)
-                    .foregroundStyle(Color.main)
-                    .cornerRadius(10, corners: [.bottomLeft, .topLeft])
-                
-                Text("빠른 예매")
-                    .font(.system(size: 17, weight: .bold))
-                    .foregroundStyle(Color.black)
+        GeometryReader { geometry in
+            NavigationLink(destination: LoadingWebView(urlToLoad: "https://www.dtryx.com/cinema/main.do?cgid=FE8EF4D2-F22D-4802-A39A-D58F23A29C1E&BrandCd=indieart&CinemaCd=000057", viewModel: viewModel)) {
+                ZStack {
+                    Rectangle()
+                        .frame(width: 100, height: 40)
+                        .foregroundStyle(Color.main)
+                        .cornerRadius(10, corners: [.bottomLeft, .topLeft])
+                    
+                    Text("빠른 예매")
+                        .font(.system(size: 17, weight: .bold))
+                        .foregroundStyle(Color.black)
+                }
             }
+            .offset(x: geometry.size.width - 100, y: geometry.size.height / 2 + Constants.bottomOffset - 60)
         }
-        .offset(x: 151, y: 318)
     }
 }
 
